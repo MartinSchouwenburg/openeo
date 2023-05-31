fdfca6813d7592ccd58585cd6b1941d89697f8b1
martin schouwenburg <m.l.schouwenburg@utwente.nl>
2023-05-31 13:29:07 +0200

    Add files via upload
    new

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from globals import globalsSingleton
+from openeocollections import OpenEOIPCollections
+from openeocapabilities import OpenEOIPCapabilities, OpenEOIPServices, OpenEOIPServiceTypes
+from openeocollection import OpenEOIPCollection
+from openeoprocessdiscovery import OpenEOIPProcessDiscovery
+from openeoresult import OpenEOIPResult
+from openeofileformats import OpenEOIPFileFormats
+from openeojobs import OpenEOIPJobs
+
+import os
+import json
+import sqlite3 as sqll3db
+
+#init part
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+CORS(app)
+api = Api(app)
+
+globalsSingleton.initGlobals()
+
+api.add_resource( OpenEOIPCollections,'/collections')
+api.add_resource( OpenEOIPCollection,'/collection/<string:name>')
+api.add_resource( OpenEOIPCapabilities,'/capabilities')
+api.add_resource( OpenEOIPProcessDiscovery,'/processes')
+api.add_resource( OpenEOIPResult, '/result')
+api.add_resource( OpenEOIPFileFormats, '/file_formats')
+api.add_resource( OpenEOIPServices, '/services')
+api.add_resource( OpenEOIPServiceTypes, '/service_types')
+api.add_resource( OpenEOIPJobs, '/jobs') 
+
+
+if __name__ == '__main__':
+    app.run()
