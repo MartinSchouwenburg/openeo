25b8d3fcc0a455d4a924028bfbb64bf882b61ab1
MLS <m.l.schouwenburg@utwente.nl>
2023-06-15 13:32:57 +0200

    broke the circular dependecy that prevented regisreroperation to work properly with workflows

--- workflow/workflow.py
+++ workflow/workflow.py
@@ -1,19 +1,18 @@
 from enum import Enum
 import uuid
-from typing import Set, Dict, Optional, Tuple, Union
 from workflow.worklfownode import WorkflowNode
 from workflow.executionnode import ExecutionNode
 from openeooperation import *
-from globals import globalsSingleton
 from constants.constants import *
 
-class Worklflow(OpenEoOperation):
+class Workflow(OpenEoOperation):
 
-    def __init__(self, process_graph):
+    def __init__(self, process_graph, getOperation):
         self.workflowGraph = {}
         self.outputNodes = []
         self.job_id = uuid.uuid4() 
         self.sourceGraph = process_graph
+        self.getOperation = getOperation
         for processKey,processValues in process_graph.items():
             grNode = WorkflowNode(processValues)
             self.workflowGraph[processKey] = grNode
@@ -26,7 +25,7 @@ class Worklflow(OpenEoOperation):
     def run(self,waituntilfinished ):
         try:
             for node in self.outputNodes:
-                exNode = ExecutionNode(node,self)
+                exNode = ExecutionNode(node,self, self.getOperation)
                 exNode.run()
                 return exNode.outputInfo
         except:
