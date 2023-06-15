ebb4ee782fc1c02ffba2ea6dbbe6f53cbf899974
MLS <m.l.schouwenburg@utwente.nl>
2023-06-15 13:34:46 +0200

    added exception handling and some cleanup in imports

--- openeoresult.py
+++ openeoresult.py
@@ -1,44 +1,42 @@
 from flask_restful import Resource
 from flask import make_response, jsonify, request, Response
-import json
-from workflow.workflow import Worklflow
-from globals import globalsSingleton
 from constants.constants import *
 from workflow.openeoprocess import OpenEOProcess
-import copy
-import uuid
-import sys
-import traceback
+
 
 class OpenEOIPResult(Resource):
     def post(self):
         request_doc = request.get_json()
-        process = OpenEOProcess(request_doc)
+        try:
+            process = OpenEOProcess(request_doc)
 
-        if process.workflow != None:
-            outputInfo = process.workflow.run(False)
+            if process.workflow != None:
+                outputInfo = process.workflow.run(False)
 
-            if outputInfo["status"]:
-                if outputInfo["datatype"] != DTRASTER:
-                    res = { "job_id" : process.workflow.job_id,
-                            "message" : "Process completed succesfully",
-                            "data" : {
-                                "type" : self.makeType(outputInfo["datatype"]),
-                                "format" : outputInfo["format"],
-                                "value" : outputInfo["value"]
-                            }
-                            }
-                    return make_response(jsonify(res),200)
-            elif outputInfo["datatype"] == DTRASTER:
-                #dummy probably not yet correct but the this how post and images work
-                with open('file_name.bin', 'rb') as file:
-                    binary_data = file.read()
-                    return Response(binary_data,
-                                    mimetype="image/tiff",
-                                    direct_passthrough=True)
+                if outputInfo["status"]:
+                    if outputInfo["datatype"] != DTRASTER:
+                        res = { "job_id" : process.workflow.job_id,
+                                "message" : "Process completed succesfully",
+                                "data" : {
+                                    "type" : self.makeType(outputInfo["datatype"]),
+                                    "format" : outputInfo["format"],
+                                    "value" : outputInfo["value"]
+                                }
+                                }
+                        return make_response(jsonify(res),200)
+                elif outputInfo["datatype"] == DTRASTER:
+                    #dummy probably not yet correct but the this how post and images work
+                    with open('file_name.bin', 'rb') as file:
+                        binary_data = file.read()
+                        return Response(binary_data,
+                                        mimetype="image/tiff",
+                                        direct_passthrough=True)
+                    
                 
-               
-            return make_response(jsonify({"job_id" : process.workflow.job_id, "job_info" :outputInfo["value"]}),404)
+                return make_response(jsonify({"job_id" : process.workflow.job_id, "job_info" :outputInfo["value"]}),404)
+        except Exception as ex:
+            return make_response(jsonify({"job_id" : 0, "job_info" :str(ex)}),404)
+        
 
     def makeType(self, tp):
         if ( tp == DTNUMBER):
