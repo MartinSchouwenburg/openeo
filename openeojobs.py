fc07d8434f86a8e7d3fd1949c02d81b7131979b8
MLS <m.l.schouwenburg@utwente.nl>
2023-06-21 13:15:36 +0200

    added user info to starting a process for later querying

--- openeojobs.py
+++ openeojobs.py
@@ -1,8 +1,9 @@
-from flask import make_response, jsonify, request
+from flask import make_response, jsonify, request, session
 from flask_restful import Resource
 from workflow.openeoprocess import OpenEOProcess
 from processmanager import globalProcessManager
 from datetime import datetime
+from userinfo import UserInfo
 
 
 
@@ -14,9 +15,10 @@ def runProcess(job):
 class OpenEOIPJobs(Resource):
     def post(self):
         request_doc = request.get_json()
+        user = UserInfo(request)
         try:
             process = OpenEOProcess(request_doc)
-            globalProcessManager.addProcess(process)
+            globalProcessManager.addProcess(user.username, process)
 
             res = { "job_id" : str(process.workflow.job_id),
                         "status" : "submitted",
@@ -30,5 +32,9 @@ class OpenEOIPJobs(Resource):
             return make_response(jsonify(res),200)
         except Exception as ex:
             return make_response(jsonify({"job_id" : 0, "job_info" :str(ex)}),404)
+        
+    def get(self):
+        return "laters"
+        
 
         
