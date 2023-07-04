5a12feeeddff81bb4e7060feb56a8e6891992b68
MLS <m.l.schouwenburg@utwente.nl>
2023-07-04 12:56:57 +0200

    add a case for jobs/process_id as this is one of the required interfaces

--- openeojobs.py
+++ openeojobs.py
@@ -7,11 +7,6 @@ from userinfo import UserInfo
 
 
 
-def runProcess(job):
-    if job.workflow != None:
-        outputInfo = job.workflow.run(False)
-
-
 class OpenEOIPJobs(Resource):
     def post(self):
         request_doc = request.get_json()
@@ -20,11 +15,11 @@ class OpenEOIPJobs(Resource):
             process = OpenEOProcess(user, request_doc)
             globalProcessManager.addProcess(process)
 
-            res = { "job_id" : str(process.workflow.job_id),
+            res = { "job_id" : str(process.job_id),
                         "status" : "submitted",
                         "submitted" : str(datetime.now()),
                         "links" : {
-                            "href" :  request.base_url + "/" + str(process.workflow.job_id),
+                            "href" :  request.base_url + "/" + str(process.job_id),
                             "rel" : 'self',
                             "type" : "application/json"
                         }
@@ -32,11 +27,28 @@ class OpenEOIPJobs(Resource):
             return make_response(jsonify(res),200)
         except Exception as ex:
             return make_response(jsonify({"job_id" : 0, "job_info" :str(ex)}),404)
-        
+
+
     def get(self):
-        user = UserInfo(request)
-        jobs = globalProcessManager.allJobs4User(user)
-        return make_response(jsonify({'jobs' : jobs}),200)
+        try:
+            user = UserInfo(request)
+            jobs = globalProcessManager.allJobs4User(user, None)
+            return make_response(jsonify({'jobs' : jobs}),200)
+        except Exception as ex:
+            return make_response(jsonify({"job_id" : 0, "job_info" :str(ex)}),404)  
+        
+
+class OpenEOIPJobs4Job(Resource):
+    def get(self, name):
+        try:
+            user = UserInfo(request)
+            jobs = globalProcessManager.allJobs4User(user, name)
+            return make_response(jsonify({'jobs' : jobs}),200)
+        except Exception as ex:
+            return make_response(jsonify({"job_id" : 0, "job_info" :str(ex)}),404)          
+        
+     
+
         
 
         
