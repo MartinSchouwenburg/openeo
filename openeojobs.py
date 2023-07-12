5ab6ce9542847448b11fb6378a420feeb3535da3
MLS <m.l.schouwenburg@utwente.nl>
2023-07-12 10:33:08 +0200

    added class for more queries and commands of jobs ( estimate, starting)

--- openeojobs.py
+++ openeojobs.py
@@ -1,10 +1,11 @@
-from flask import make_response, jsonify, request, session
+from flask import make_response, jsonify, request
 from flask_restful import Resource
 from workflow.openeoprocess import OpenEOProcess
 from processmanager import globalProcessManager
 from datetime import datetime
 from userinfo import UserInfo
-
+import socket
+import json
 
 
 class OpenEOIPJobs(Resource):
@@ -37,16 +38,38 @@ class OpenEOIPJobs(Resource):
         except Exception as ex:
             return make_response(jsonify({"job_id" : 0, "job_info" :str(ex)}),404)  
         
-
-class OpenEOIPJobs4Job(Resource):
-    def get(self, name):
+            
+        
+class OpenEOIJobById2(Resource):
+   def post(self, job_id):
+        request_doc = request.get_json()
+        if not 'id' in request_doc:
+            return make_response(jsonify({"job_id" : 0, "job_info" :str("missing \'job_id\' key in definition")}),404)  
+        
+        user = UserInfo(request)
+        message = globalProcessManager.queueJob(user, job_id)
+        return make_response(jsonify({"job_id" : 0, "job_info" :message}),404)   
+    
+class OpenEOIJobByIdEstimate(Resource):
+   def get(self, job_id):
+        user = UserInfo(request)
+        estimate = globalProcessManager.makeEstimate(user, job_id)
+        costs = estimate[0][2]
+        return make_response(jsonify(costs),estimate[1])  
+        
+class OpenEOIJobById(Resource):
+    def get(self, job_id):
         try:
             user = UserInfo(request)
-            jobs = globalProcessManager.allJobs4User(user, name)
+            jobs = globalProcessManager.allJobs4User(user, job_id)
             return make_response(jsonify({'jobs' : jobs}),200)
         except Exception as ex:
             return make_response(jsonify({"job_id" : 0, "job_info" :str(ex)}),404)          
-        
+
+    def delete(self, job_id):
+         user = UserInfo(request)
+         globalProcessManager.stopJob(user, job_id)
+         return make_response(jsonify({"job_id" : job_id, "job_info" :str('has been successfully deleted')}),204) 
      
 
         
