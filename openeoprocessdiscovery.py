cbfc027c6e7ddf2d6e487dd8a7a189d67d1c50a4
MLS <m.l.schouwenburg@utwente.nl>
2023-09-05 10:10:10 +0200

    semantics of this test have changed

--- openeoprocessdiscovery.py
+++ openeoprocessdiscovery.py
@@ -2,12 +2,16 @@ from flask_restful import Resource
 from flask import make_response, jsonify, request
 from globals import globalsSingleton
 from processmanager import linkSection
+from constants import constants
 
 class OpenEOIPProcessDiscovery(Resource):
+    def __init__(self):
+        Resource.__init__(self)
+
     def get(self):
         operations = []
         for operation in globalsSingleton.operations.values() :
-            if hasattr(operation, 'predefined') and operation.predefined:
+            if hasattr(operation, 'kind') and operation.kind == constants.PDPREDEFINED:
                 operations.append(operation.toDict())
         processes = {'processes' : operations}  
         processes["links"]  = linkSection(request.base_url, 'processes')
