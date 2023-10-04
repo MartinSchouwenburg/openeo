8afcd20df4998295e1d029ddd5cc097519b96712
MLS <m.l.schouwenburg@utwente.nl>
2023-10-04 13:24:57 +0200

    mainly refactoring; moving some stuff to eooperation

--- operations/load_collection.py
+++ operations/load_collection.py
@@ -37,49 +37,48 @@ class LoadCollectionOperation(OpenEoOperation):
                     self.bandIdxs.append(0)
             else:
                 self.bandIdxs.append(0)
-            
+
             if 'temporal_extent' in arguments:
+                self.temporalExtent = arguments['temporal_extent']
                 if arguments['temporal_extent'] != None:
                     self.lyrIdxs = self.inputRaster.getLayerIndexes(arguments['temporal_extent'])
                 else:
                     self.lyrIdxs.append(0)                
 
-            #csycode = "epsg:" + str(r.epsg)
-
             path = Path(folder).as_uri()
             ilwis.setWorkingCatalog(path)
             self.runnable = True
-            ##grf = ilwis.GeoReference(csycode, ilwis.Envelope(env) , ilwis.Size(15,12))
+            self.rasterSizesEqual = True
  
         except Exception as ex:
             return ""
 
         return ""
-    
-
-              
-
+   
     def run(self, job_id, processOutput, processInput):
         if self.runnable:
+            outputRasters = []
             indexes = str(self.bandIdxs).lstrip('[').rstrip(']')
-            bandIdxList = 'rasterbands(' + indexes + ')'
             ext = self.inputRaster.spatialExtent
             env = str(ext[0]) + " " + str(ext[2]) + "," + str(ext[1]) + " " +str(ext[3])
-            rasters = []
-            for lyrIdx in self.lyrIdxs:
-                layer = self.inputRaster.idx2layer(lyrIdx)
-                if layer != None:    
-                    datapath = os.path.join(self.dataSource, layer.dataSource)
-                    rband = ilwis.RasterCoverage(datapath)
-                    rc = ilwis.do("selection", rband, "envelope(" + env + ") with: " + bandIdxList)
-                    rasterData = RasterData()
-                    extra = { 'temporalExtent' : layer.temporalExtent, 'bands' : self.inputRaster.index2bands(indexes), 'epsg' : self.inputRaster.epsg}
-                    rasterData.fromRasterCoverage(rc, extra )
-                    rasters.append(rasterData)
+            for idx in indexes:
+                bandIdxList = 'rasterbands(' + idx + ')'
+                rasters = []
+                for lyrIdx in self.lyrIdxs:
+                    layer = self.inputRaster.idx2layer(lyrIdx)
+                    if layer != None:    
+                        datapath = os.path.join(self.dataSource, layer.dataSource)
+                        rband = ilwis.RasterCoverage(datapath)
+                        rc = ilwis.do("selection", rband, "envelope(" + env + ") with: " + bandIdxList)
+                        rasters.append(rc)
+
+                extra = self.constructExtraParams(self.inputRaster, self.temporalExtent, idx)
+                outputRasters.extend(self.setOutput(rasters, extra))
+
+
+            return createOutput('finished', outputRasters, constants.DTRASTER)
         
-            return createOutput('finished', rasters, constants.DTRASTER)
-        
-        return createOutput('error', "operation no runnable", constants.DTERROR)
-        
+        return createOutput('error', "operation not runnable", constants.DTERROR)
+           
 def registerOperation():
      return LoadCollectionOperation()
