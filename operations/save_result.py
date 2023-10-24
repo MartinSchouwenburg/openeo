cb534a32edd6931e4aa116093557ddaedbfcaed8
MLS <m.l.schouwenburg@utwente.nl>
2023-10-24 13:18:19 +0200

    added piece to rertrieve correctly generated files. gdal sometimes adds extesnion which are not in the original name so we gather what is generated

--- operations/save_result.py
+++ operations/save_result.py
@@ -11,9 +11,9 @@ class SaveResultOperation(OpenEoOperation):
 
     def prepare(self, arguments):
         self.runnable = False
-        self.format = arguments['format']
+        self.format = arguments['format']['resolved']
         self.data = arguments['data']['resolved']
-        self.options = arguments['options']
+        self.options = arguments['options']['resolved']
         self.runnable = True
         return ""
               
@@ -24,13 +24,23 @@ class SaveResultOperation(OpenEoOperation):
             path = openeoip_config['data_locations']['root_user_data_location']
             path = path['location'] + '/' + str(job_id)    
             os.makedirs(path)
-            for d in self.data:
-                name = d.ilwisRaster.name()
-                name = name.replace('_ANONYMOUS', 'raster')
-                outpath = path + '/' + name
-                d.ilwisRaster.store("file://" + outpath,self.format, "gdal")
-            self.logEndOperation(processOutput, job_id)                
-            return createOutput('finished', None, constants.DTRASTER)
+            if self.data != None:
+                for d in self.data:
+                    name = d.ilwisRaster.name()
+                    name = name.replace('_ANONYMOUS', 'raster')
+                    outpath = path + '/' + name
+                    d.ilwisRaster.store("file://" + outpath,self.format, "gdal")
+                
+                self.logEndOperation(processOutput, job_id) 
+
+                ext = ('.tif','.dat','.mpr','.tiff','.jpg', '.png')
+                file_names = [f for f in os.listdir(path) if f.endswith(ext)]
+                files = []
+                for filename in file_names:
+                    fn = path + "/"  + filename
+                    files.append(fn)
+
+            return createOutput('finished', files, constants.DTRASTERLIST)
         
         return createOutput('error', "operation no runnable", constants.DTERROR)
         
