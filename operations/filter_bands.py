0107a0ca6aaba800e2ecdb4363f10e15d66e5ce9
MLS <m.l.schouwenburg@utwente.nl>
2023-10-24 13:18:30 +0200

    new

---
+++ operations/filter_bands.py
@@ -0,0 +1,46 @@
+from openeooperation import *
+from operationconstants import *
+from constants import constants
+from rasterdata import RasterData
+import ilwis
+
+class FilterBands(OpenEoOperation):
+    def __init__(self):
+        self.loadOpenEoJsonDef('filter_bands.json') 
+
+    def prepare(self, arguments):
+        try:
+            self.runnable = False 
+            self.inpData = arguments['data']['resolved']
+            if len(self.inpData) == 0:
+                return 'invalid input. Number of rasters is 0'            
+            if isinstance(self.inpData[0], RasterData):
+                requestedBands = arguments['bands']['resolved']
+                foundCount = 0
+                for item in self.inpData:
+                    for bandItem in item.bands:
+                        if bandItem['name'] in requestedBands:
+                            foundCount = foundCount + 1
+                if foundCount == len(requestedBands):
+                    self.bands = requestedBands                    
+                    self.runnable = True
+                else:
+                    return 'Band list doesn match available bands'                    
+
+        except:
+            return "error" 
+
+    def run(self, job_id, processOutput, processInput):
+        if self.runnable:
+            outData = []
+            for raster in self.inpData:
+                    for bandItem in raster.bands:
+                        if bandItem['name'] in self.bands:
+                            outData.append(raster)
+
+            return createOutput('finished', outData, constants.DTRASTER)
+        
+        return createOutput('error', "operation not runnable", constants.DTERROR)   
+
+def registerOperation():
+    return FilterBands()               
