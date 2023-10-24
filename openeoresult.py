b2016002b52ff9344499f9ebfdffd64976fa8f8d
MLS <m.l.schouwenburg@utwente.nl>
2023-10-24 13:20:49 +0200

    correct mime type for several image types

--- openeoresult.py
+++ openeoresult.py
@@ -4,8 +4,26 @@ from constants.constants import *
 from workflow.openeoprocess import OpenEOProcess
 from userinfo import UserInfo
 from processmanager import makeBaseResponseDict
+from io import BytesIO
+from zipfile import ZipFile
+import os
+from werkzeug.wsgi import FileWrapper
+import pathlib
 
-
+def getMimeType(filename):
+    try:
+        ext = pathlib.Path(filename).suffix
+        if ext == '.jpg':
+            return "image/jpeg"
+        elif ext == '.tif' or ext == '.tiff':
+            return 'image/tiff'
+        elif ext == '.png':
+            return "image/png" 
+        return 'application/octet-stream'
+    except:
+        return 'application/octet-stream'
+          
+        
 class OpenEOIPResult(Resource):
     def post(self):
         request_json = request.get_json()
@@ -16,19 +34,32 @@ class OpenEOIPResult(Resource):
             if process.processGraph != None:
                 outputInfo = process.processGraph.run(process.job_id, None, None)
 
-                if outputInfo["status"]:
-                    if outputInfo["datatype"] != DTRASTER:
+                if outputInfo["status"] == STATUSFINISHED:
+                    if outputInfo["datatype"] == DTRASTER or outputInfo["datatype"] == DTRASTERLIST :
+                        if len(outputInfo["value"]) ==1:
+                            filename = outputInfo["value"][0]
+                            mimet = getMimeType(filename)
+                            with open(filename, 'rb') as file:
+                                binary_data = file.read()
+                                response = Response(binary_data,
+                                                mimetype=mimet,
+                                                direct_passthrough=True)
+                        else:   
+                            stream = BytesIO()
+                            with ZipFile(stream, 'w') as zf:                                                         
+                                for fn in outputInfo["value"]:
+                                    zf.write(file, os.path.basename(file))
+                                stream.seek(0)
+                                w = FileWrapper(stream)
+                                response = Response(w,
+                                                mimetype="application/x-zip",
+                                                direct_passthrough=True)
+
+                          
+                    elif outputInfo["datatype"] != DTRASTER:
                         response = Response(str(outputInfo["value"]), mimetype = "string", direct_passthrough=True)
                         response.headers['Content-Type'] = 'string'
-                        return response
-                elif outputInfo["datatype"] == DTRASTER:
-                    #dummy probably not yet correct but the this how post and images work
-                    with open(outputInfo["value"], 'rb') as file:
-                        binary_data = file.read()
-                        response = Response(binary_data,
-                                        mimetype="image/tiff",
-                                        direct_passthrough=True)
-                        return response
+                    return response
                     
                 response =  make_response(makeBaseResponseDict(process.job_id, 'error', 404, None, outputInfo["value"]),400)
                 response.headers['Content-Type'] = 'string'
