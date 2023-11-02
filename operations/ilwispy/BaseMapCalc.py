1a27934d0e446664ec250bd3e1d4c3d0261b08b7
MLS <m.l.schouwenburg@utwente.nl>
2023-11-02 14:02:32 +0100

    handles now list of rasters

--- operations/ilwispy/BaseMapCalc.py
+++ operations/ilwispy/BaseMapCalc.py
@@ -2,6 +2,7 @@ from openeooperation import *
 from operationconstants import *
 from constants import constants
 import math
+from rasterdata import RasterData
 
 class BaseUnarymapCalc(OpenEoOperation):
     def base_prepare(self, arguments, oper):
@@ -9,22 +10,25 @@ class BaseUnarymapCalc(OpenEoOperation):
             self.runnable = False
             self.rasterSizesEqual = True
 
-            if len(arguments) != 2:
+            if len(arguments) != 1:
                 return  createOutput(False,"number of parameters is not correct",  constants.DTERROR)
             it = iter(arguments)
-            self.p1 = arguments[next(it)]['resolved']
-            self.mapcalc = type(self.p1) is RasterData
-
-            if not type(self.p1) is RasterData:
-                if math.isnan(self.p1):
+            p1 = arguments[next(it)]['resolved']
+            if isinstance(p1, list):
+                rasterList = []
+                for ras in p1:
+                    if type(ras) is RasterData:
+                        extra = self.constructExtraParams(ras, ras.temporalExtent, 0)
+                        raster = ras.getRaster().rasterImp()
+                        rasterList.append({'raster' : raster, 'extra' : extra})
+                    self.parmValue = rasterList                        
+                        
+            else:
+                if math.isnan(p1):
                     return createOutput(False, "the parameter a is not a number", constants.DTERROR)
-            
-            self.runnable = True
-
-            if type(self.p1) is RasterData:
-                self.extra = self.constructExtraParams(self.p1, self.p1.temporalExtent, 0)
-                self.p1 = self.p1.getRaster().rasterImp()
-            self.operation = oper                           
+                self.parmValue = p1                        
+            self.operation = oper
+            self.runnable = True                                      
                 
         except Exception as ex:
             return ""
@@ -33,15 +37,16 @@ class BaseUnarymapCalc(OpenEoOperation):
         if self.runnable:
 
             put2Queue(processOutput, {'progress' : 0, 'job_id' : job_id, 'status' : 'running'})
+            if isinstance(self.parmValue, list):
+                outputRasters = []                                
+                for item in self.parmValue:
+                    oper = self.operation + '(@1)'
+                    outputRc = ilwis.do('mapcalc', oper, item['raster'])
 
-            if self.mapcalc:
-                oper = self.operation + '(@2)'
-                outputRc = ilwis.do('mapcalc', oper, self.p1)
-                outputRasters = []                
-                outputRasters.extend(self.setOutput([outputRc], self.extra))
+                outputRasters.extend(self.setOutput([outputRc], item['extra']))
                 out =  createOutput('finished', outputRasters, constants.DTRASTER)                
             else:
-                c = self.a * self.b
+                c = eval(self.operation + '(' + self.parmValue + ')')
                 out = createOutput('finished', c, constants.DTNUMBER)
 
             put2Queue(processOutput,{'progress' : 100, 'job_id' : job_id, 'status' : 'finished'}) 
