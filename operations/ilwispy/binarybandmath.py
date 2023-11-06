bff99697fe4fb718ee1ff0749366d3b61163e685
MLS <m.l.schouwenburg@utwente.nl>
2023-11-06 10:08:49 +0100

    added new functions

--- operations/ilwispy/binarybandmath.py
+++ operations/ilwispy/binarybandmath.py
@@ -42,14 +42,122 @@ class SubtractOperation(BaseBinarymapCalcBase):
         return ""
 
     def run(self, job_id, processOutput, processInput):
-        return self.base_run(job_id, processOutput, processInput)      
-       
+        return self.base_run(job_id, processOutput, processInput) 
+
+class LogNOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('log.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'logn')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class GTOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('gt.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, '>')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class GTEOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('gte.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, '>=')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class LTOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('lt.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, '<')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class LTEOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('lte.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, '<=')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class EqOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('eq.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, '==')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class OrOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('or.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'or')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class AndOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('and.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'and')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class XorOperation(BaseBinarymapCalcBase):
+    def __init__(self):
+        self.loadOpenEoJsonDef('xor.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'xor')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput)                                      
+    
 def registerOperation():
     funcs = []     
     funcs.append(AddOperation())
     funcs.append(DivideOperation())
     funcs.append(MultiplyOperation()) 
-    funcs.append(SubtractOperation())           
+    funcs.append(SubtractOperation()) 
+    funcs.append(LogNOperation()) 
+    funcs.append(GTOperation()) 
+    funcs.append(GTEOperation()) 
+    funcs.append(LTOperation()) 
+    funcs.append(LTEOperation()) 
+    funcs.append(EqOperation()) 
+    funcs.append(OrOperation())
+    funcs.append(AndOperation()) 
+    funcs.append(XorOperation())                                        
 
 
     return funcs
--- operations/ilwispy/unarybandmath.py
+++ operations/ilwispy/unarybandmath.py
@@ -11,6 +11,28 @@ class ArcCosOperation(BaseUnarymapCalc):
     def run(self, job_id, processOutput, processInput):
         return self.base_run(job_id, processOutput, processInput)
     
+class ArcCosHOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('arcosh.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'acosh')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class CosHOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('cosh.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'cosh')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput)         
+    
 class CosOperation(BaseUnarymapCalc):
     def __init__(self):
         self.loadOpenEoJsonDef('cos.json')
@@ -32,6 +54,17 @@ class ASinOperation(BaseUnarymapCalc):
 
     def run(self, job_id, processOutput, processInput):
         return self.base_run(job_id, processOutput, processInput)
+    
+class ASinHOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('arsinh.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'asinh')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput)    
         
 class SinOperation(BaseUnarymapCalc):
     def __init__(self):
@@ -43,6 +76,17 @@ class SinOperation(BaseUnarymapCalc):
 
     def run(self, job_id, processOutput, processInput):
         return self.base_run(job_id, processOutput, processInput)
+    
+class SinHOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('sinh.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'sinh')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput)    
 
 class TanOperation(BaseUnarymapCalc):
     def __init__(self):
@@ -54,6 +98,17 @@ class TanOperation(BaseUnarymapCalc):
 
     def run(self, job_id, processOutput, processInput):
         return self.base_run(job_id, processOutput, processInput)
+    
+class TanHOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('tanh.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'tanh')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput)    
 
 class ATanOperation(BaseUnarymapCalc):
     def __init__(self):
@@ -64,15 +119,91 @@ class ATanOperation(BaseUnarymapCalc):
         return ""
 
     def run(self, job_id, processOutput, processInput):
-        return self.base_run(job_id, processOutput, processInput)             
+        return self.base_run(job_id, processOutput, processInput) 
+
+class AbsOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('absolute.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'abs')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput)                  
+    
+class CeilOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('ceil.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'ceil')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class FloorOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('floor.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'floor')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput)
+
+class IntOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('int.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'int')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class RoundOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('round.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'round')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput) 
+
+class LnOperation(BaseUnarymapCalc):
+    def __init__(self):
+        self.loadOpenEoJsonDef('ln.json')
+
+    def prepare(self, arguments):
+        self.base_prepare(arguments, 'ln')
+        return ""
+
+    def run(self, job_id, processOutput, processInput):
+        return self.base_run(job_id, processOutput, processInput)                       
 
 def registerOperation():
      funcs = []
      funcs.append(ArcCosOperation())
+     funcs.append(ArcCosHOperation())     
      funcs.append(CosOperation())     
      funcs.append(SinOperation()) 
-     funcs.append(ASinOperation()) 
+     funcs.append(ASinOperation())
+     funcs.append(ASinHOperation())      
      funcs.append(TanOperation()) 
-     funcs.append(ATanOperation())                         
+     funcs.append(ATanOperation()) 
+     funcs.append(AbsOperation()) 
+     funcs.append(CeilOperation())                                     
+     funcs.append(IntOperation())      
+     funcs.append(RoundOperation()) 
+     funcs.append(CosHOperation())          
+     funcs.append(SinHOperation())      
+     funcs.append(TanHOperation())
+     funcs.append(LnOperation())             
 
      return funcs
