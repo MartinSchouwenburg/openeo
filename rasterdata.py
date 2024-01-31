cc535926f774ef3ba5eaa757b13fd6c93fcc7249
MartinSchouwenburg <m.l.schouwenburg@utwente.nl>
2024-01-31 10:05:40 +0100

    a raster can write itself now as a .metadata file

--- rasterdata.py
+++ rasterdata.py
@@ -69,7 +69,7 @@ class RasterData:
         self.lastmodified = datetime.fromtimestamp(mttime)
         prod = Reader().open(filepath)
         self.stac_version = '1.0'
-        self.type = 'File'
+        self.type = 'file'
         namepath = os.path.splitext(filepath)[0]
         head, tail = os.path.split(namepath)
         self.id = tail
@@ -83,18 +83,21 @@ class RasterData:
         time = [str(prod.stac.datetime), str(prod.stac.datetime)]
         self.temporalExtent = time
         self.dataSource = filepath
-        self.dataFolder = ""
+        self.dataFolder = head
         self.epsg = prod.stac.proj.epsg
         self.spatialExtent = prod.stac.proj.bbox
         self.summaries= {}
         self.setSummariesValue('constellation', prod.stac)
         self.setSummariesValue('instrument', prod)
         self.clouds  = prod.get_cloud_cover()
+        self.grouping = 'band'
         
-        bands = prod.bands._band_map
+       # bands = prod.bands._band_map
+       # b = prod.bands.items()
         self.bands = []
+        index = 0
         defnames = ['name', 'common_name', 'description', 'center_wavelength', 'full_width_half_max', 'solar_illumination','gsd']
-        for band in bands.items():
+        for band in prod.bands.items():
             b = band[1]
             if ( b != None):
                 att = {"type" : "float"}
@@ -109,12 +112,17 @@ class RasterData:
                                 details[key] = value
                 if name != '':                            
                     att['name'] = name
+                b = band[0] 
+                att['normalizedbandname'] = b.value
                 att["details"] = details
-                self.bands.append(att)
+                att["index"] = index
+                self.bands.append(att)            
+            index = index + 1                
         self.layers = []
         layer = RasterLayer()
         layer.temporalExtent = self.temporalExtent   
         layer.dataSource = self.dataSource
+        layer.index = 0
         self.layers.append(layer)
 
         
@@ -166,6 +174,36 @@ class RasterData:
         self.layers.append(lyr)
         self.raster = RasterImplementation(rc2)
   
+    def toMetadataFile(self, folder):
+        filename = os.path.join(folder, self.id + ".metadata")
+        meta = {}
+        meta['stac_version'] = self.stac_version
+        meta['type'] = 'Collection'
+        meta['title'] = self.title
+        meta['id'] = self.id
+        meta['description'] = self.description
+        meta['license'] = self.license
+        meta['keywords'] = self.keywords
+        meta['providers'] = self.providers
+        meta['links'] = self.links
+        meta['projection'] = { 'epsg': self.epsg}
+        meta['grouping'] = 'band'
+        dimensions = {}
+        dimensions['bounding_box'] = self.boundingbox
+        dimensions['t'] = [{'extent': self.temporalExtent, 'source' : 'all'}]
+        dimensions['t'].append({'extent': self.temporalExtent, 'source' : self.id + ".metadata"})
+        dimensions['x'] = {'extent' : [self.spatialExtent[0], self.spatialExtent[1]], 'reference_system' : self.epsg}
+        dimensions['y'] = {'extent' : [self.spatialExtent[2], self.spatialExtent[3]], 'reference_system' : self.epsg}
+        dimensions['bands'] = self.bands        
+        meta['dimensions'] = dimensions
+        head = os.path.split(self.dataFolder)
+        meta['data_folder'] = head[1]
+        if hasattr(self, 'summaries'):
+            meta['summaries'] = self.summaries
+
+        with open(filename, "w") as write_file:
+                    json.dump(meta, write_file, indent=4) 
+        return filename                    
 
     def fromMetadataFile(self, filepath):
         metafile = open(filepath)
@@ -181,6 +219,7 @@ class RasterData:
         self.keywords = self.getValue('keywords', metadata, [])
         self.providers = self.getValue('providers', metadata, 'unknown')
         self.links = self.getMandatoryValue("links", metadata) 
+        self.grouping = self.getValue("grouping", metadata, "band") 
         ext = self.getMandatoryValue("dimensions", metadata)
         self.boundingbox = self.getMandatoryValue("bounding_box", ext)
         self.epsg = self.getValue('epsg' , metadata['projection'], '0')
@@ -233,7 +272,8 @@ class RasterData:
     def toLongDictDefinition(self):
         dictDef = self.toShortDictDefinition()
         dictDef['cube:dimensions'] = self.getJsonExtent()
-        dictDef['summaries'] = {"constellation" : self.summaries["constellation"], "instrument" : self.summaries['instrument']}
+        if hasattr(self, 'summaries'):
+            dictDef['summaries'] = {"constellation" : self.summaries["constellation"], "instrument" : self.summaries['instrument']}
         if hasattr(self, 'clouds'):
             dictDef['eo:cloud_cover'] = [0, self.clouds]
         if hasattr(self, 'snow'):
@@ -245,6 +285,7 @@ class RasterData:
         for b in self.bands:
                 if ( b != None):
                     bdef = {"name": b['name']}
+                    bdef['normalizedbandname'] = b['normalizedbandname']
                     for kvp in b['details'].items():
                        bdef[kvp[0]] = kvp[1]
                        if kvp[0] == 'gsd':
@@ -321,13 +362,13 @@ class RasterData:
     def getBandIndexes(self, requestedBands):
         idxs = []
         for reqBandName in requestedBands:
+            idx = 0 
             for b in self.bands:
-                idx = 0 
-                if b['name'] == reqBandName:
+                if b['name'] == reqBandName or b['normalizedbandname'] == reqBandName:
                     if 'index' in b:
                         idxs.append(b['index'])
                     else:                    
-                        idxs.append[idx]
+                        idxs.append(idx)
                 idx = idx + 1
         return idxs           
 
